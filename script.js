66481ff3308214690cf8f6038c3d59d2a7c7da54
Joshua Norris <joshua.norris52@gmail.com>
2024-04-18 11:12:45 -0500

    Adding JS funcitonality (mostly just tying in the api).

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('testing');
+
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element then play
+async function selectMediaStream() {
+    try {
+        const medioStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = medioStream;
+        videoElement.onloadedmetadata = () => { videoElement.play(); }
+        // this is true when it finishes loading
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
+
+// On load
+selectMediaStream();
